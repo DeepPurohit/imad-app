ccb7d57772b0524d9ce284ead4441d0c66eb3f99
Deep Purohit <DeepPurohit@users.noreply.github.com>
2018-02-16 20:47:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article-one', function(req, res){
     res.send('Article one is requested');
 });
 
-pp.get('/article-two', function(req, res){
+app.get('/article-two', function(req, res){
     res.send('Article two is requested');
 });
 
-pp.get('/article-three', function(req, res){
+app.get('/article-three', function(req, res){
     res.send('Article three is requested');
 });
 
