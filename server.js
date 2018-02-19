ddff8edc156037ae6306d6e814900388daf07b42
Deep Purohit <DeepPurohit@users.noreply.github.com>
2018-02-19 21:39:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var artcles= {
+var articles= {
     'article-one' : {
         title: 'Article one | Deep Purohit',
         heading: 'Article one',
