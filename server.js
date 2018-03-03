6d3831ff7ee3cc0a5a4092899f6f0e0b3319468f
Deep Purohit <DeepPurohit@users.noreply.github.com>
2018-03-03 20:22:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+
+var config = {
+    user : 'purohitdeep05',
+    database : 'purohitdeep05',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -80,6 +90,19 @@ var htmlTemplate =
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+
+app.get('/test-db', function(req, res){
+    pool.query('Select * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 app.get('/:articleName', function(req, res){
