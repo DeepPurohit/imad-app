c8ad4b0a9182754893d06e3671f4f7261ab32a88
Deep Purohit <DeepPurohit@users.noreply.github.com>
2018-03-06 23:42:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,12 +97,12 @@ app.get('/article/:articleName', function(req, res){
 
 function hash(input){
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+     return hashedString.toString('hex');
 }
 
 app.get('/hash/:input', function(req, res){
     var hashedString = hash(req.params.input, 'This-is-some-random-string');
     res.send(hashedString);
-    return hashedString.toString('hex');
 });
 
 var counter = 0;
